083e49f11b36eb3761a7c0c65a4dc7168a070e92
Juan Diego Fierro <fierrojuan2005@gmail.com>
2025-08-10 19:45:28 -0500

    Builder

---
+++ Tarea3/src/main/java/ec/edu/espol/tarea3/Cliente.java
@@ -0,0 +1,25 @@
+package ec.edu.espol.tarea3;
+
+public class Cliente {
+    private ManejadorIncidente manejadorIncidentes;
+
+    public Cliente(ManejadorIncidente manejadorIncidentes) {
+        this.manejadorIncidentes = manejadorIncidentes;
+    }
+
+    // Método para crear reservas
+    public Reserva crearReserva() {
+        PaqueteBuilder builder = new PaqueteConcreteBuilder();
+        return builder
+            .agregarHabitacion("101", 80.0)
+            .agregarPaseo("Tour Histórico", 45.0)
+            .agregarExtra(new SpaDecorator(new Habitacion("101", 80.0), 25.0))
+            .construir();
+    }
+
+    // Método para reportar incidentes
+    public Resultado reportarIncidente(String descripcion) {
+        Incidente incidente = new Incidente("INC-" + System.currentTimeMillis(), descripcion);
+        return manejadorIncidentes.manejar(incidente);
+    }
+}
