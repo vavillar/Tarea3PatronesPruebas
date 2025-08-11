093bc305292fedd7e0bd752edc792942546e0162
vit2005-newbie <vavillar@espol.edu.ec>
2025-08-10 20:27:39 -0500

    Creacion de archivos test
    Creacion de archivos test

---
+++ Tarea3/src/test/java/ec/edu/espol/tarea3/ComponenteTest.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ec.edu.espol.tarea3;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Victor
+ */
+public class ComponenteTest {
+    
+    public ComponenteTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of precio method, of class Componente.
+     */
+    @Test
+    public void testPrecio() {
+        System.out.println("precio");
+        Componente instance = new ComponenteImpl();
+        double expResult = 0.0;
+        double result = instance.precio();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of estaDisponible method, of class Componente.
+     */
+    @Test
+    public void testEstaDisponible() {
+        System.out.println("estaDisponible");
+        Componente instance = new ComponenteImpl();
+        boolean expResult = false;
+        boolean result = instance.estaDisponible();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    public class ComponenteImpl implements Componente {
+
+        public double precio() {
+            return 0.0;
+        }
+
+        public boolean estaDisponible() {
+            return false;
+        }
+    }
+    
+}
---
+++ Tarea3/src/test/java/ec/edu/espol/tarea3/ObservadorReservaTest.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ec.edu.espol.tarea3;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Victor
+ */
+public class ObservadorReservaTest {
+    
+    public ObservadorReservaTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of actualizar method, of class ObservadorReserva.
+     */
+    @Test
+    public void testActualizar() {
+        System.out.println("actualizar");
+        Reserva reserva = null;
+        ObservadorReserva instance = new ObservadorReservaImpl();
+        instance.actualizar(reserva);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    public class ObservadorReservaImpl implements ObservadorReserva {
+
+        public void actualizar(Reserva reserva) {
+        }
+    }
+    
+}
